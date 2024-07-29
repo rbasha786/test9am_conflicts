1270d59c77a24667f3e0dece9ae16a6cbecce273
SohamKwame <RiyazM.Basha37@gmail.com>
2024-07-29 09:42:49 +0530

    progaram from Kwame

---
+++ hello.java
@@ -0,0 +1,7 @@
+class Test   //Program developed by Kwame
+  {
+    public static void main(String []args)  // Main class
+    {
+        System.out.println("First Java Program from Kwame");
+    }
+ };
