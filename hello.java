18b82801c0f2e0603991d9f789f49c971d50f531
SohamKwame <RiyazM.Basha37@gmail.com>
2024-07-29 09:42:08 +0530

    hello program from Soham

---
+++ hello.java
@@ -0,0 +1,7 @@
+ class Test   //Program developed by Soham
+ {
+     public static void main(String []args)  // Main class
+    {
+       System.out.println("My First Java Program from Soham.");
+    }
+ };
